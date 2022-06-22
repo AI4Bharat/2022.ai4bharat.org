f1816fd40c9aae1770ec478599c166abc2bbc0b5
A-HK <kahani.0142@gmail.com>
2022-06-22 15:35:00 +0530

    modified mobile nav

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -729,13 +729,13 @@ export const Header = ({ data }) => {
           <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
             <div className="pt-5 pb-6 px-5">
               <div className="flex items-center justify-between">
-                <div>
+                {/* <div>
                   <img
                     className="h-8 w-auto"
                     src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                     alt="Workflow"
                   />
-                </div>
+                </div> */}
                 <div className="-mr-2">
                   <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                     <span className="sr-only">Close menu</span>
@@ -745,20 +745,21 @@ export const Header = ({ data }) => {
               </div>
               <div className="mt-6">
                 <nav className="grid gap-y-8">
-                  {solutions.map((item) => (
+                  {data.nav && data.nav.map((item) => (
                     <a
-                      key={item.name}
-                      href={item.href}
+                    key={item.label}
+                    href={item.href}
                       className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
                     >
-                      <item.icon className="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
-                      <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
+                     
+                     
+                      <span className="ml-3 text-base font-medium text-gray-900">{item.label}</span>
                     </a>
                   ))}
                 </nav>
               </div>
             </div>
-            <div className="py-6 px-5 space-y-6">
+            {/* <div className="py-6 px-5 space-y-6">
               <div className="grid grid-cols-2 gap-y-4 gap-x-8">
                 <a href="/home" className="text-base font-medium text-gray-900 hover:text-gray-700">
                   Home
@@ -767,7 +768,7 @@ export const Header = ({ data }) => {
                 <a href="/positions" className="text-base font-medium text-gray-900 hover:text-gray-700">
                   Positions
                 </a>
-                {resources.map((item) => (
+                {data.resources.map((item) => (
                   <a
                     key={item.name}
                     href={item.href}
@@ -777,21 +778,8 @@ export const Header = ({ data }) => {
                   </a>
                 ))}
               </div>
-              <div>
-                <a
-                  href="#"
-                  className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                >
-                  Sign up
-                </a>
-                <p className="mt-6 text-center text-base font-medium text-gray-500">
-                  Existing customer?{' '}
-                  <a href="#" className="text-indigo-600 hover:text-indigo-500">
-                    Sign in
-                  </a>
-                </p>
-              </div>
-            </div>
+            
+            </div> */}
           </div>
         </Popover.Panel>
       </Transition>
