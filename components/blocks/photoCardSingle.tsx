14bcf052ca7aece4958d4006debca748be76e0e1
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2022-07-10 18:50:15 +0530

    reduce the spacing

--- components/blocks/photoCardSingle.tsx
+++ components/blocks/photoCardSingle.tsx
@@ -5,16 +5,16 @@ import type { TinaTemplate } from "tinacms";
 const PhotoCard = ({ featuresColor, data, tinaField }) => {
   return (
     <div data-tinafield={tinaField} className="w-full md:w-1/2 xl:w-1/3 px-4" >
-      <div className="bg-white rounded-lg overflow-hidden mb-10">
+      <div className="bg-white rounded-lg overflow-hidden">
         <img className="w-full" src={data.image} alt="" />
 
-        <div className="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
-          <h3 className="font-semibold text-dark text-xl sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px] mb-4 block hover:text-primary"
+        <div className="p-2 text-center">
+          <h3 className="font-semibold text-dark text-xl sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px] mb-2 block hover:text-primary"
           data-tinafield={`${tinaField}.title`}
           >
             {data.title}
           </h3>
-          <p className="text-base text-body-color leading-relaxed mb-7" data-tinafield={`${tinaField}.text`}>
+          <p className="text-base text-body-color leading-relaxed" data-tinafield={`${tinaField}.text`}>
             {data.text}
           </p>
         </div>
@@ -25,11 +25,11 @@ const PhotoCard = ({ featuresColor, data, tinaField }) => {
 
 export const PhotoCardSingle = ({ data, parentField }) => {
   return (
-    <Section color={data.color} className="pb-12">
+    <Section color={data.color}>
 
       <Container
-        className="p-10 gap-10 flex justify-center items-center"
-        size="small"
+        className="p-5 gap-5 flex justify-center items-center"
+        size="py-1"
       >
           {data.items?.map((block, i) => 
             <>
