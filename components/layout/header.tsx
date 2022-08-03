8539d103553abf7b360504615e783a36ce64dd45
Ishvinder Sethi <ishvindersethi22@gmail.com>
2022-08-03 08:30:08 +0530

    Update header.tsx

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -294,7 +294,77 @@ export const Header = ({ data }) => {
               )}
             </Popover>
 
-           
+           <Popover className="relative">
+              {({ open }) => (
+                <>
+                  <Popover.Button
+                    className={classNames(
+                      open ? 'text-gray-900' : 'text-gray-500',
+                      'group font-semibold rounded-md inline-flex items-center text-sm hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2'
+                    )}
+                  >
+                    <span>About us</span>
+                    <ChevronDownIcon
+                      className={classNames(
+                        open ? 'text-gray-600' : 'text-gray-400',
+                        'ml-2 h-5 w-5 group-hover:text-gray-500'
+                      )}
+                      aria-hidden="true"
+                    />
+                  </Popover.Button>
+
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-200"
+                    enterFrom="opacity-0 translate-y-1"
+                    enterTo="opacity-100 translate-y-0"
+                    leave="transition ease-in duration-150"
+                    leaveFrom="opacity-100 translate-y-0"
+                    leaveTo="opacity-0 translate-y-1"
+                  >
+                    <Popover.Panel className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-xs sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
+                      <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                        <div className="relative grid  gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+       
+                          {/* {console.log(data.callsToAction)} */}
+                         
+                          {data.about && data.about.map((item) => (
+                            <a
+                              key={item.label}
+                              href={item.href}
+                              className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
+                            >
+                              {(item.icon &&
+                               <Icon
+                                  parentColor={data.color}
+                                  data={{
+                                    size: "xs",
+                                    name: item.icon.name,
+                                    color: item.icon.color,
+                                    style: item.icon.style,
+                                  }}
+                                  className="flex-shrink-0 h-6 w-6"
+                                /> 
+                            )}
+                              <div className="ml-4">
+                                <p className="text-sm font-semibold text-gray-900">{item.label}</p>
+                                <p className="mt-1 text-sm text-gray-500">{item.description}</p>
+                        
+                              </div>
+                            </a>
+                          ))
+                          }
+                        </div>
+                     
+                        <div className="px-5 py-5 bg-gray-50 space-y-6 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
+       
+                        </div>
+                      </div>
+                    </Popover.Panel>
+                  </Transition>
+                </>
+              )}
+            </Popover>
             <a href="/positions" className="text-sm font-semibold text-gray-500 hover:text-gray-900">
               Positions
             </a>
